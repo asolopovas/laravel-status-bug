27d30f319362eb4f8cdd81c7e4bfff69609bec56
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-06-10 17:48:47 +0100

    save

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="3 Oak Tm Wood Flooring Ltd. DataBase">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Test</title>
+</head>
+<body>
+<div id="app">Test</div>
+</body>
+</html>
